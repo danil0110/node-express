f83de544cdc26c3f89e8231cc42f9eaa6466c8f3
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-04-02 19:50:18 +0300

    Added routing for a course's details

--- models/course.js
+++ models/course.js
@@ -53,6 +53,11 @@ class Course {
             );
         })
     }
+
+    static async getById(id) {
+        const courses = await Course.getAll();
+        return courses.find(item => item.id === id);
+    }
 }
 
 module.exports = Course;
--- routes/courses.js
+++ routes/courses.js
@@ -9,6 +9,14 @@ router.get('/', async (req, res) => {
         isCourses: true,
         courses
     });
+});
+
+router.get('/:id', async (req, res) => {
+    const course = await Course.getById(req.params.id);
+    res.render('course', {
+        title: `Курс ${course.title}`,
+        course
+    });
 })
 
 module.exports = router;
