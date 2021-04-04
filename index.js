0d90be3fca4e4a5eae3c396cfb8507bdfb7aba16
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-04-04 21:45:11 +0300

    Added userId field for course model

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const coursesRoutes = require('./routes/courses');
 const cartRoutes = require('./routes/cart');
 const addRoutes = require('./routes/add');
 
+const User = require('./models/user');
+
 const app = express();
 const hbs = exphbs.create({
     defaultLayout: 'main',
@@ -20,6 +22,17 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
+
+
+app.use(async (req, res, next) => {
+    try {
+        const user = await User.findById('606a041ff4d5b57e6b6aa0f8');
+        req.user = user;
+        next();
+    } catch (e) {
+        console.log(e);
+    }
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
@@ -40,6 +53,15 @@ async function start() {
             useFindAndModify: false,
             useUnifiedTopology: true
         });
+        const candidate = await User.findOne();
+        if (!candidate) {
+            const user = new User({
+                email: 'test@gmail.com',
+                name: 'John Week',
+                cart: {items: []}
+            });
+            await user.save();
+        }
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}...`);
         });
--- routes/add.js
+++ routes/add.js
@@ -11,7 +11,12 @@ router.get('/', (req, res) => {
 
 router.post('/', async (req, res) => {
     const {title, price, img} = req.body;
-    const course = new Course({ title, price, img });
+    const course = new Course({
+        title,
+        price,
+        img,
+        userId: req.user._id
+    });
 
     try {
         await course.save();
