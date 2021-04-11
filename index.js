53780fc3b53473c52df1a1d01161bde9e8162824
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-04-11 20:55:03 +0300

    Deleted unnecessary code

--- index.js
+++ index.js
@@ -27,15 +27,6 @@ app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 
-app.use(async (req, res, next) => {
-    try {
-        const user = await User.findById('606a041ff4d5b57e6b6aa0f8');
-        req.user = user;
-        next();
-    } catch (e) {
-        console.log(e);
-    }
-});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
@@ -64,15 +55,6 @@ async function start() {
             useFindAndModify: false,
             useUnifiedTopology: true
         });
-        const candidate = await User.findOne();
-        if (!candidate) {
-            const user = new User({
-                email: 'test@gmail.com',
-                name: 'John Week',
-                cart: {items: []}
-            });
-            await user.save();
-        }
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}...`);
         });
