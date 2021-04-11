3d9c08fb4084e51931eeebb591bdf21ad899d74a
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-04-11 20:53:48 +0300

    Added session saving

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const {Router} = require('express');
+const User = require('../models/user');
 const router = Router();
 
 router.get('/login', async (req, res) => {
@@ -9,8 +10,13 @@ router.get('/login', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
+    const user = await User.findById('606a041ff4d5b57e6b6aa0f8');
+    req.session.user = user;
     req.session.isAuthenticated = true;
-    res.redirect('/');
+    req.session.save((err) => {
+        if (err) throw err;
+        res.redirect('/');
+    });
 });
 
 router.get('/logout', async (req, res) => {
