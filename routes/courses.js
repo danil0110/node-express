09ad00429aadd318a718da060f45fa8bc4f15cd6
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-04-13 20:38:29 +0300

    Added secure routing

--- routes/courses.js
+++ routes/courses.js
@@ -1,5 +1,6 @@
 const {Router} = require('express');
 const Course = require('../models/course');
+const auth = require('../middleware/auth');
 
 const router = Router();
 router.get('/', async (req, res) => {
@@ -24,14 +25,14 @@ router.get('/:id/edit', async (req, res) => {
     });
 });
 
-router.post('/edit', async (req, res) => {
+router.post('/edit', auth, async (req, res) => {
     const {id} = req.body;
     delete req.body.id;
     await Course.findByIdAndUpdate(id, req.body);
     res.redirect('/courses');
 });
 
-router.post('/remove', async (req, res) => {
+router.post('/remove', auth, async (req, res) => {
     try {
         await Course.deleteOne({ _id: req.body.id });
         res.redirect('/courses');
