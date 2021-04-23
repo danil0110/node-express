c0e3441e96a3474e9334ced65d8ce92cad2122d1
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-04-23 21:13:29 +0300

    Added SendGrid API key

--- keys/index.js
+++ keys/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     MONGODB_URI: 'mongodb+srv://danil0110:oEADX8zLPqBYFiRF@cluster0.8iyiy.mongodb.net/shop',
     SESSION_SECRET: 'some secret value',
+    SENDGRID_API_KEY: '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'
 }
