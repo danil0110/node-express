a97984120e1eee3eb40239d4462bcbb18975a970
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-04-23 21:43:01 +0300

    Added email sending after registration

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,15 @@
 const {Router} = require('express');
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgrid = require('nodemailer-sendgrid-transport');
 const User = require('../models/user');
+const keys = require('../keys');
+const regEmail = require('../emails/registration');
 const router = Router();
+
+const transporter = nodemailer.createTransport(sendgrid({
+    auth: { api_key: keys.SENDGRID_API_KEY }
+}));
 
 router.get('/login', async (req, res) => {
     res.render('auth/login', {
@@ -65,6 +73,7 @@ router.post('/register', async (req, res) => {
             });
             await newUser.save();
             res.redirect('/auth/login#login');
+            await transporter.sendMail(regEmail(email));
         } else {
             req.flash('registerError', 'Пароли не совпадают, повторите попытку.');
             res.redirect('/auth/login#register');
