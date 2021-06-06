c8664dcdc3f9f10a532b8579b57a70e82209d14f
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-06-06 17:25:58 +0300

    Added security to routes

--- routes/courses.js
+++ routes/courses.js
@@ -3,13 +3,18 @@ const Course = require('../models/course');
 const auth = require('../middleware/auth');
 
 const router = Router();
+
+function isOwner(course, req) {
+	return course.userId.toString() === req.user._id.toString();
+}
+
 router.get('/', async (req, res) => {
 	try {
 		const courses = await Course.find().populate('userId', 'email name');
 		res.render('courses', {
 			title: 'Курсы',
 			isCourses: true,
-            userId: req.user ? req.user._id.toString() : null,
+			userId: req.user ? req.user._id.toString() : null,
 			courses,
 		});
 	} catch (e) {
@@ -22,24 +27,47 @@ router.get('/:id/edit', async (req, res) => {
 		return res.redirect('/');
 	}
 
-	const course = await Course.findById(req.params.id);
+	try {
+		const course = await Course.findById(req.params.id);
 
-	res.render('course-edit', {
-		title: `Редактировать ${course.title}`,
-		course,
-	});
+		if (!isOwner(course, req)) {
+			return res.redirect('/courses');
+		}
+
+		res.render('course-edit', {
+			title: `Редактировать ${course.title}`,
+			course,
+		});
+	} catch (e) {
+		console.log(e);
+	}
 });
 
 router.post('/edit', auth, async (req, res) => {
-	const { id } = req.body;
-	delete req.body.id;
-	await Course.findByIdAndUpdate(id, req.body);
-	res.redirect('/courses');
+	try {
+		const { id } = req.body;
+		delete req.body.id;
+		const course = await Course.findById(id);
+
+		if (!isOwner(course, req)) {
+			return res.redirect('/courses');
+		}
+
+		Object.assign(course, req.body);
+		await course.save();
+		res.redirect('/courses');
+	} catch (e) {
+		console.log(e);
+	}
 });
 
 router.post('/remove', auth, async (req, res) => {
 	try {
-		await Course.deleteOne({ _id: req.body.id });
+		await Course.deleteOne({
+			_id: req.body.id,
+			userId: req.user._id,
+		});
+
 		res.redirect('/courses');
 	} catch (e) {
 		console.log(e);
@@ -47,13 +75,16 @@ router.post('/remove', auth, async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-	console.log('ID', req.params.id);
-	const course = await Course.findById(req.params.id);
-	res.render('course', {
-		layout: 'empty',
-		title: `Курс ${course.title}`,
-		course,
-	});
+	try {
+		const course = await Course.findById(req.params.id);
+		res.render('course', {
+			layout: 'empty',
+			title: `Курс ${course.title}`,
+			course,
+		});
+	} catch (e) {
+		console.log(e);
+	}
 });
 
 module.exports = router;
