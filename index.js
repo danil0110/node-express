4b47a6978d1babfc188f2d600d89fc65db739eff
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-06-06 17:13:01 +0300

    Show edit button only for course owner

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const app = express();
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs',
+    helpers: require('./utils/hbs-helpers'),
     handlebars: allowInsecurePrototypeAccess(Handlebars)
 });
 const store = new MongoStore({
