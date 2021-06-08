5d86043879f1efade76f8046e652347a666303ec
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-06-08 17:19:32 +0300

    Added avatar upload feature

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const profileRoutes = require('./routes/profile');
 const varMiddleware = require('./middleware/variables');
 const userMiddleware = require('./middleware/user');
 const errorHandler = require('./middleware/error');
+const fileMiddleware = require('./middleware/file');
 const keys = require('./keys');
 
 const app = express();
@@ -41,6 +42,7 @@ app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.urlencoded({ extended: true }));
 app.use(
 	session({
@@ -50,6 +52,7 @@ app.use(
 		store,
 	})
 );
+app.use(fileMiddleware.single('avatar'));
 app.use(csrf());
 app.use(flash());
 
