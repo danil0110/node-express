26294a0087c3c92231646bfff2fc9c94c5c45649
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-06-08 18:17:41 +0300

    Keys split to dev and prod

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,5 @@
-module.exports = {
-    MONGODB_URI: 'mongodb+srv://danil0110:Uj80iPQXuh0GsoCU@cluster0.8iyiy.mongodb.net/shop',
-    SESSION_SECRET: 'some secret value',
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: 'courses-shop@nodecourses.com',
-    BASE_URL: 'http://localhost:3000'
+if (process.env.NODE_ENV === 'production') {
+	module.exports = require('./keys.prod');
+} else {
+	module.exports = require('./keys.dev');
 }
---
+++ keys/keys.dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+	MONGODB_URI:
+		'mongodb+srv://danil0110:Uj80iPQXuh0GsoCU@cluster0.8iyiy.mongodb.net/shop',
+	SESSION_SECRET: 'some secret value',
+	EMAIL_FROM: 'node-courses@gmail.com',
+	BASE_URL: 'http://localhost:3000',
+};
